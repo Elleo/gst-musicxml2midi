cb09176ed65ae662ff1881a55396a2474ffc6613
Mike Sheldon <mike@mikeasoft.com>
2010-08-16 14:01:02 +0100

    Revert "Rename MusicXml2Midi class to MusicXml2MidiParser so that the decoders_filter accepts us as a valid decoding plugin when auto-plugging"
    This reverts commit be989e7e4efc975bf7aa0b8defc714efd9d768f8.

--- src/gstmusicxml2midi.h
+++ src/gstmusicxml2midi.h
@@ -53,22 +53,22 @@
 G_BEGIN_DECLS
 
 /* #defines don't like whitespacey bits */
-#define GST_TYPE_MUSICXML2MIDIPARSER \
+#define GST_TYPE_MUSICXML2MIDI \
   (gst_musicxml2midi_get_type())
-#define GST_MUSICXML2MIDIPARSER(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_MUSICXML2MIDIPARSER,GstMusicXml2MidiParser))
+#define GST_MUSICXML2MIDI(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_MUSICXML2MIDI,GstMusicXml2Midi))
 #define GST_MUSICXML2MIDI_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_MUSICXML2MIDIPARSER,GstMusicXml2MidiParserClass))
-#define GST_IS_MUSICXML2MIDIPARSER(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_MUSICXML2MIDIPARSER))
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_MUSICXML2MIDI,GstMusicXml2MidiClass))
+#define GST_IS_MUSICXML2MIDI(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_MUSICXML2MIDI))
 #define GST_IS_MUSICXML2MIDI_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_MUSICXML2MIDIPARSER))
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_MUSICXML2MIDI))
 
-typedef struct _GstMusicXml2MidiParser      GstMusicXml2MidiParser;
-typedef struct _GstMusicXml2MidiParserClass GstMusicXml2MidiParserClass;
+typedef struct _GstMusicXml2Midi      GstMusicXml2Midi;
+typedef struct _GstMusicXml2MidiClass GstMusicXml2MidiClass;
 typedef struct _Track                 Track;
 
-struct _GstMusicXml2MidiParser
+struct _GstMusicXml2Midi
 {
   GstElement element;
 
@@ -80,7 +80,7 @@ struct _GstMusicXml2MidiParser
   xmlParserCtxtPtr ctxt;
 };
 
-struct _GstMusicXml2MidiParserClass
+struct _GstMusicXml2MidiClass 
 {
   GstElementClass parent_class;
 };
